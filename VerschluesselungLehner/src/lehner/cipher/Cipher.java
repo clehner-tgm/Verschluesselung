e189522535d5e97007b64a2f24fcb3059d351c45
Cindy Lehner <clehner@student.tgm.ac.at>
2014-04-06 15:07:06 +0200

    Grundaufbau und die ersten Methoden

---
+++ VerschluesselungLehner/src/lehner/cipher/Cipher.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package lehner.cipher;
+
+/**
+ * @author Cindy Lehner
+ * @version 2014-04-06
+ */
+public interface Cipher {
+
+}
---
+++ VerschluesselungLehner/src/lehner/cipher/MonoAlphabeticCipher.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package lehner.cipher;
+
+/**
+ * @author Cindy Lehner
+ * @version 2014-04-06
+ */
+public class MonoAlphabeticCipher {
+
+	//Attribute
+	private String secretAlphabet;
+	
+	//Konstruktor
+	public MonoAlphabeticCipher(){
+		
+	}
+	
+	//Weitere Methoden
+	
+	 /**
+	  * Diese Methode gibt das geheime Alphabet zurück
+	  * @return  das Geheimalphabet
+	  */
+	public String getSecretAlphabet(){
+		String r = "";
+		return r;
+	}
+	
+	 /**
+	  * Diese Methode setzt das geheime Alphabet
+	  * @param secretAlphabet  das neue Alphabet
+	  */
+	protected void setSecretAlphabet(String secretAlphabet){
+		
+	}
+}
