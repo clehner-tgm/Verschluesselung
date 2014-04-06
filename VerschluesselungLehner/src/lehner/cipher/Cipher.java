7c08cd206600bcaa902b5852e9c06367fdfda7b2
Cindy Lehner <clehner@student.tgm.ac.at>
2014-04-06 15:19:53 +0200

    Interface Cipher fertig

--- VerschluesselungLehner/src/lehner/cipher/Cipher.java
+++ VerschluesselungLehner/src/lehner/cipher/Cipher.java
@@ -1,12 +1,28 @@
-/**
- * 
- */
 package lehner.cipher;
 
 /**
+ * Das Interface mit den Methodendeklarationen, die zum ver- und entschlüsseln dienen.
+ * 
  * @author Cindy Lehner
  * @version 2014-04-06
  */
+
 public interface Cipher {
 
+	/**
+	 * Methodendeklaration, die einen übergebenen String verschlüsselt
+	 * 
+	 * @param text  der übergebene normale Stringwert
+	 * @return  der verschlüsselte Wert
+	 */
+	public String encrypt(String text);
+	
+	/**
+	 * Methodendeklaration, die einen übergebenen String entschlüsselt
+	 * 
+	 * @param text  der übergebene verschlüsselte Stringwert
+	 * @return  der entschlüsselte Wert
+	 */
+	public String decrypt(String text);
+	
 }
